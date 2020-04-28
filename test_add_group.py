c79799a5b160f67d0bb452a24136fa179cf43cb6
Mari <ak_mariya@tut.by>
2020-04-28 16:31:43 +0300

    Refactor code. Made new methods

--- test_add_group.py
+++ test_add_group.py
@@ -14,15 +14,19 @@ class TestAddGroup(unittest.TestCase):
     
     def test_add_group(self):
         driver = self.driver
-        driver.get("http://localhost/addressbook/group.php")
-        driver.find_element_by_id("LoginForm").click()
-        driver.find_element_by_name("user").click()
-        driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys("admin")
-        driver.find_element_by_name("pass").click()
-        driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys("secret")
-        driver.find_element_by_xpath("//input[@value='Login']").click()
+        self.open_main_page(driver)
+        self.login(driver)
+        self.add_new_group(driver)
+        self.open_group_page(driver)
+        self.logout(driver)
+
+    def logout(self, driver):
+        driver.find_element_by_link_text("Logout").click()
+
+    def open_group_page(self, driver):
+        driver.find_element_by_link_text("group page").click()
+
+    def add_new_group(self, driver):
         driver.find_element_by_name("new").click()
         driver.find_element_by_name("group_name").click()
         driver.find_element_by_name("group_name").clear()
@@ -32,19 +36,20 @@ class TestAddGroup(unittest.TestCase):
         driver.find_element_by_name("group_footer").clear()
         driver.find_element_by_name("group_footer").send_keys("Comment")
         driver.find_element_by_name("submit").click()
-        driver.find_element_by_link_text("group page").click()
-        driver.find_element_by_link_text("Logout").click()
-    
-    def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.driver.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
+
+    def login(self, driver):
+        driver.find_element_by_id("LoginForm").click()
+        driver.find_element_by_name("user").click()
+        driver.find_element_by_name("user").clear()
+        driver.find_element_by_name("user").send_keys("admin")
+        driver.find_element_by_name("pass").click()
+        driver.find_element_by_name("pass").clear()
+        driver.find_element_by_name("pass").send_keys("secret")
+        driver.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_main_page(self, driver):
+        driver.get("http://localhost/addressbook/group.php")
+
     def tearDown(self):
         self.driver.quit()
 
