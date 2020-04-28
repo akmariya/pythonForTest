aa3db107f68ad08d84c48433d9bc6bbc96dd9fd0
Mari <ak_mariya@tut.by>
2020-04-28 16:46:57 +0300

    Add new class Group

---
+++ group.py
@@ -0,0 +1,7 @@
+
+
+class Group():
+    def __init__(self, name, logo, comment):
+        self.name = name
+        self.logo = logo
+        self.comment = comment
--- test_add_group.py
+++ test_add_group.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
+from group import Group
+
 
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
@@ -15,8 +12,8 @@ class TestAddGroup(unittest.TestCase):
     def test_add_group(self):
         driver = self.driver
         self.open_main_page(driver)
-        self.login(driver)
-        self.add_new_group(driver)
+        self.login(driver, login="admin", password="secret")
+        self.add_new_group(driver, Group("Some name","Some logo", "Some comment"))
         self.open_group_page(driver)
         self.logout(driver)
 
@@ -26,25 +23,25 @@ class TestAddGroup(unittest.TestCase):
     def open_group_page(self, driver):
         driver.find_element_by_link_text("group page").click()
 
-    def add_new_group(self, driver):
+    def add_new_group(self, driver, group):
         driver.find_element_by_name("new").click()
         driver.find_element_by_name("group_name").click()
         driver.find_element_by_name("group_name").clear()
-        driver.find_element_by_name("group_name").send_keys("Some Name")
+        driver.find_element_by_name("group_name").send_keys(group.name)
         driver.find_element_by_name("group_header").clear()
-        driver.find_element_by_name("group_header").send_keys("Logo")
+        driver.find_element_by_name("group_header").send_keys(group.logo)
         driver.find_element_by_name("group_footer").clear()
-        driver.find_element_by_name("group_footer").send_keys("Comment")
+        driver.find_element_by_name("group_footer").send_keys(group.comment)
         driver.find_element_by_name("submit").click()
 
-    def login(self, driver):
+    def login(self, driver, login, password):
         driver.find_element_by_id("LoginForm").click()
         driver.find_element_by_name("user").click()
         driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys("admin")
+        driver.find_element_by_name("user").send_keys(login)
         driver.find_element_by_name("pass").click()
         driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys("secret")
+        driver.find_element_by_name("pass").send_keys(password)
         driver.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_main_page(self, driver):
