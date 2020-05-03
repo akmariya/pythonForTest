898bd82dd9c5676c3b8a61a262854e75bcbb50a9
Mari <ak_mariya@tut.by>
2020-05-04 00:34:20 +0300

    Add test_edit_first_group

---
+++ test/test_edit_first_group.py
@@ -0,0 +1,10 @@
+import time
+
+from model.group import Group
+
+
+def test_edit_first_group(app):
+    app.session.login(login="admin", password="secret")
+    app.group.edit_first_group(Group("New name", "New logo", "New comment"))
+    time.sleep(10)
+    app.session.logout()
