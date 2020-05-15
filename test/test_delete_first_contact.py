0b83d68cd379c63ecb43caf0ead0e73db705fae4
Mari <ak_mariya@tut.by>
2020-05-15 21:10:36 +0300

    More intellectual test with contacts

--- test/test_delete_first_contact.py
+++ test/test_delete_first_contact.py
@@ -1,4 +1,7 @@
+from model.contact import Contact
 
 
-def test_add_new_contact(app):
+def test_delete_first_contact(app):
+    if app.contact.count() == 0:
+        app.contact.add(Contact(firstname="New Contact"))
     app.contact.delete_first_contact()
--- test/test_edit_first_contact.py
+++ test/test_edit_first_contact.py
@@ -2,6 +2,8 @@ from model.contact import Contact
 
 
 def test_edit_first_contact(app):
+    if app.contact.count() == 0:
+        app.contact.add(Contact(firstname="New Contact"))
     app.contact.edit_first_contact(Contact(firstname="New name", middlename="Ivanovich", lastname="Ivanov", nickname="Vanua", title="Some title",
                             company="Some company", address="Adress", home="Home", mobile=375, work=546, fax=9866, email="vanua@tut.by", email2="vanua2@tut.by",
                             email3="vanua3@tut.by", homepage="vanya.com", bday="17", bmonth="January", byear="1996", aday="26", amonth="April", ayear="2000",
