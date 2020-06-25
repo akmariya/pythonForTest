3d0bae74789f5cbbfe1e6555c34b30946fab2d99
Mari <ak_mariya@tut.by>
2020-06-26 00:01:33 +0300

    Group tests with reading from json

--- test/test_edit_some_group.py
+++ test/test_edit_some_group.py
@@ -1,29 +1,15 @@
-import pytest
 from model.group import Group
 import random
-import string
 
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Group(name="", logo="", comment="")] + [
-    Group(name=random_string("name", 10), logo=random_string("logo", 10), comment=random_string("comment", 10))
-    for i in range(5)
-]
-
-
-@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
-def test_edit_some_group(app, group):
+def test_edit_some_group(app, json_groups):
     if app.group.count() == 0:
         app.group.create(Group(name="New group"))
     old_groups = app.group.get_group_list()
     index = random.randrange(len(old_groups))
-    group.id = old_groups[index].id
-    app.group.edit_group_by_index(group, index)
+    json_groups.id = old_groups[index].id
+    app.group.edit_group_by_index(json_groups, index)
     assert len(old_groups) == app.group.count()
     new_groups = app.group.get_group_list()
-    old_groups[index] = group
+    old_groups[index] = json_groups
     assert sorted(old_groups, key=Group.id_or_max), sorted(new_groups, key=Group.id_or_max)
